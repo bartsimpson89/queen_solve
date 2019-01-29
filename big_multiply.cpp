68324f3a815535dc7b49d5512088461eb673c04b
bartsimpson89 <shu_nan2008@163.com>
2019-01-29 21:16:51 +0800

    refactor: refectore big_multiply

--- big_multiply.cpp
+++ big_multiply.cpp
@@ -33,6 +33,59 @@ int freeList(struct dataNode *head)
 	free(tmp);
 	return 0;
 }
+struct dataNode *copyList(struct dataNode *src)
+{
+	struct dataNode *src_head = src;
+	struct dataNode *head = NULL;
+	struct dataNode *result = NULL;
+	while(src->next!=NULL)
+	{
+		struct dataNode *tmpNode = (struct dataNode *)malloc(sizeof(struct dataNode));
+		tmpNode->next=NULL;
+		tmpNode->pre=NULL;
+		tmpNode->data=src->data;
+		if (src == src_head)
+		{
+			head=tmpNode;
+		}
+		else
+		{
+			tmpNode->pre = result;
+			result->next=tmpNode;
+		}
+		result = tmpNode;
+		src=src->next;
+	}
+               
+	struct dataNode *tmpNode = (struct dataNode *)malloc(sizeof(struct dataNode));
+	tmpNode->data = src->data;
+	tmpNode->next = NULL;
+	tmpNode->pre = result;
+	result->next = tmpNode;
+	result = tmpNode;
+	return head;
+}
+
+int compareListLen(struct dataNode *head1, struct dataNode *head2)
+{
+	while(head1->next!=NULL&&head2->next!=NULL)
+	{
+		head1 = head1->next;
+		head2 = head2->next;
+	}
+	return (head1->next == NULL) ? 0 : 1;
+}
+
+struct dataNode *newNode(struct dataNode *preNode, int data)
+{
+	struct dataNode *tmpNode = (struct dataNode *)malloc(sizeof(struct dataNode));
+	tmpNode->next = NULL;
+	tmpNode->pre = preNode;
+	tmpNode->data = data;
+	if (preNode != NULL)
+		preNode->next = tmpNode;
+	return tmpNode;
+}
 
 struct dataNode *addList(struct dataNode *head1,struct dataNode *head2)
 {
@@ -42,176 +95,95 @@ struct dataNode *addList(struct dataNode *head1,struct dataNode *head2)
 	struct dataNode *p_short = NULL;
 	if (head1 == NULL || head2 == NULL)
 	{
-		p1 = (head1==NULL)?head2:head1;
-		struct dataNode *head = NULL;
-		struct dataNode *result = NULL;
-		while(p1->next!=NULL)
-		{
-			struct dataNode *tmpNode = (struct dataNode *)malloc(sizeof(struct dataNode));
-			tmpNode->next=NULL;
-			tmpNode->pre=NULL;
-			tmpNode->data=p1->data;
-			if(p1 == head1 || p1 == head2)
-			{
-				head=tmpNode;
-			}
-			else
-			{
-				tmpNode->pre = result;
-				result->next=tmpNode;
-
-			}
-			result = tmpNode;
-			p1=p1->next;
-		}
-               
-		struct dataNode *tmpNode = (struct dataNode *)malloc(sizeof(struct dataNode));
-		tmpNode->data = p1->data;
-		tmpNode->next=NULL;
-		tmpNode->pre = result;
-		result->next=tmpNode;
-		result=tmpNode;
-
-		return head;
-
+		p1 = (head1 == NULL) ? head2:head1;
+		return copyList(p1);
 	}
-
-	while(p1->next!=NULL&&p2->next!=NULL)
-	{
-		p1=p1->next;
-		p2=p2->next;
-	}
-	if (p1->next == NULL)
-	{
-		p_long = head2;
-		p_short = head1;
-	}
-	else 
+	if (compareListLen(head1, head2) > 0)
 	{
 		p_long = head1;
 		p_short = head2;
 	}
-	int tmp = 0;
-
-	struct dataNode *head = NULL;
-	struct dataNode *result = NULL;
-	while (p_long->next!=NULL)
+	else
 	{
-		struct dataNode *tmpNode = (struct dataNode *)malloc(sizeof(struct dataNode));
-		tmpNode->next=NULL;
-		tmpNode->pre=NULL;
+		p_long = head2;
+		p_short = head1;
+	}
 
+	
+	int remainder = 0;
+	int data = 0;
+	struct dataNode *head = NULL;
+	struct dataNode *list = NULL;
+	while (p_long->next != NULL)
+	{
 		if(p_short!=NULL)		
 		{
-			tmpNode->data = (p_long->data+p_short->data+tmp)%10;
-			tmp = (p_long->data+p_short->data+tmp)/10;
+			data = (p_long->data + p_short->data+remainder) % 10;
+			remainder = (p_long->data + p_short->data+remainder) / 10;
 		}
 		else
 		{
-			tmpNode->data = (p_long->data+tmp)%10;
-			tmp = (p_long->data+tmp)/10;
+			data = (p_long->data + remainder) % 10;
+			remainder = (p_long->data + remainder) / 10;
 		}
 
 		if (p_long == head1 || p_long == head2)
-		{
-			head = tmpNode;
-		}
+			list = head = newNode(list,data);
 		else
-		{
-			tmpNode->pre=result;
-			result->next=tmpNode;
-			tmpNode->next=NULL;
-		}
-		result = tmpNode;
-		p_long = p_long->next;
-		p_short = p_short->next;
+			list = newNode(list, data);
+
+		p_long = p_long->next; p_short = p_short->next;
 	}
-	struct dataNode *tmpNode = (struct dataNode *)malloc(sizeof(struct dataNode));
-	tmpNode->data = (p_long->data + tmp)%10;
-	tmp = (p_long->data + tmp)/10;
-	tmpNode->next=NULL;
-	tmpNode->pre = result;
-	result->next=tmpNode;
-	result=tmpNode;
+
+	data = (p_long->data + remainder) % 10;
+	remainder = (p_long->data + remainder) / 10;
+	list = newNode(list, data);
 	
-	
-	if (tmp > 0)
-	{
-		struct dataNode *tmpNode = (struct dataNode *)malloc(sizeof(struct dataNode));
-		tmpNode->data = tmp;
-		tmpNode->next = NULL;
-		tmpNode->pre = result;
-		result->next=tmpNode;
-	}
+	if (remainder > 0)
+		list = newNode(list, data);
 
 	return head;
 }
 	        
 struct dataNode *getResult(int a[],int size,int b,int left)
 {
-	int tmp = 0;
-	int result = 0;
+	int remainder = 0;
+	int data = 0;
 	struct dataNode *head = NULL;
-	struct dataNode *tmpNode = NULL;
+	struct dataNode *list = NULL;
+
+	for(int i=0;i<left;i++)	
+		if (i == 0)
+			list = head = newNode(list, 0);
+		else 
+			list = newNode(list, 0);
 
 	for(int i=size-1;i>=0;i--)
 	{
-
-		result = (b*a[i]+tmp)%10;
-		tmp = (b*a[i]+tmp)/10;
-		struct dataNode *p = (struct dataNode *)malloc(sizeof(struct dataNode));
-		p->data = result;
-		p->next=NULL;
-		p->pre=NULL;
-		
-		if (i == size -1)
-		{
-			head = p;
-		}
-		else
-		{
-			p->pre = tmpNode;
-			tmpNode->next = p;
-		}
-		tmpNode = p;
+		data = (b*a[i] + remainder) % 10;
+		remainder = (b*a[i] + remainder) / 10;
+		if (head == NULL)
+			list = head = newNode(list, data);
+		else 
+			list = newNode(list, data);
 	}
-	if (tmp > 0)
-	{
-		struct dataNode *p = (struct dataNode *)malloc(sizeof(struct dataNode));
-		p->data = tmp;
-		p->pre = tmpNode;
-		tmpNode->next = p;
-		p->next = NULL;
-	}
-
+	if (remainder > 0)
+		list = newNode(list, remainder);
 	
-	for(int i=0;i<left;i++)
-	{
-		
-		struct dataNode *p = (struct dataNode *)malloc(sizeof(struct dataNode));
-		p->data = 0;
-		p->pre=NULL;
-
-		head->pre=p;
-		p->next=head;
-		head=p;
-	}
-
 	return head;
 }
 
-
-
 int main(void)
 {
-	int data[6]={1,2,3,4,5,6};
-	int data2[3]={1,1,1};
+	int data[6]={6,6,6,7,8,9};
+	int data2[4]={9,9,9,9};
+	int data2_len = sizeof(data2)/sizeof(data2[0]);
+	int data_len =  sizeof(data)/sizeof(data[0]);
 	struct dataNode *result2 = NULL;
-	for(int i=sizeof(data2)/sizeof(data2[0])-1;i>=0;i--)
+	for(int i=data2_len - 1; i>=0;i--)
 	{
-		struct dataNode *result = getResult(data,sizeof(data)/sizeof(data[0]),data2[i],sizeof(data2)/sizeof(data2[0])-i-1);
-		printList(result);
-		
+		struct dataNode *result = getResult(data, data_len, data2[i], data2_len-i-1);
+
 		result2 = addList(result, result2);
 
 		printList(result2);
